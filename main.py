ceef9e2a7dd9a9f86f2f979c86fecdcfecad9b7f
Parker Moesta <75709283+DimensionDweller@users.noreply.github.com>
2023-05-10 19:26:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import pandas as pd
+import nltk
 from get_news import get_news
 from clean_news import clean_text
 from sentiment_analysis import get_sentiment
 from visualizations import generate_visualizations
 
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+
+NEWSAPI = 'YOUR_NEWSAPI_KEY_HERE'
+query_term = 'climate change'
+
 def main():
     # Collect data
-    df = get_news()
+    df = get_news(NEWSAPI, query_term)
 
     # Preprocess data
     df = clean_text(df)
@@ -19,3 +27,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
