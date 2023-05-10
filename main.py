adcf3f07b05f87d788a2cd742b89f271967a7f4a
Parker Moesta <75709283+DimensionDweller@users.noreply.github.com>
2023-05-10 19:03:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pandas as pd
-from data_collection import collect_data
-from preprocessing import clean_text
+from get_news import get_news
+from clean_news import clean_text
 from sentiment_analysis import get_sentiment
 from visualizations import generate_visualizations
 
 def main():
     # Collect data
-    df = collect_data()
+    df = get_news()
 
     # Preprocess data
     df = clean_text(df)
